77785f61a2dd8952fcae1a7adf51bfd13afcce39
YRYK1985 <ryutaro.910.tigers@icloud.com>
2026-03-17 12:15:08 +0900

    update

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,13 @@ export const metadata = {
     title: '東海オンエア 動画バトル',
     description: '3000本以上の東海オンエア動画から好きな方を選んで投票しよう！',
     type: 'website',
+    images: [{ url: 'https://tokairanking.com/ogp.png', width: 1200, height: 630 }],
   },
   twitter: {
     card: 'summary_large_image',
     title: '東海オンエア 動画バトル',
     description: '3000本以上の東海オンエア動画から好きな方を選んで投票しよう！',
+    images: ['https://tokairanking.com/ogp.png'],
   },
 };
 
@@ -23,7 +25,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700;900&display=swap" rel="stylesheet" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9862215132601373" crossOrigin="anonymous"></script>
       </head>
       <body style={{ margin: 0, padding: 0 }}>{children}</body>
